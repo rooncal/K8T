2fb39df742272e3b545800629bc5a8bc2c99b020
רון קלקודה <roon@rwns-MacBook-Pro.local>
2024-03-15 15:50:55 +0200

    Add nodes & vars

--- Terraform/cluster_ec2.tf
+++ Terraform/cluster_ec2.tf
@@ -1,8 +1,38 @@
-data "aws_ami" "amzn-linux-2023-ami" {
+resource "aws_subnet" "cluster_subnet" {
+  vpc_id            = aws_vpc.cluster_vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.subnet_az
+
+  tags = {
+    Name = "k8s-cluster-network"
+  }
+}
+
+data "aws_ami" "ubuntu-ami" {
   most_recent = true
 
   filter {
     name   = "image-id"
-    values = ["ami-03035978b5aeb1274"]
+    values = [var.k8s_ami]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "cluster-key"
+  public_key = var.ssh_public_key
+}
+
+resource "aws_instance" "node" {
+  count = 3
+  ami           = data.aws_ami.ubuntu-ami.id
+  instance_type = var.aws_k8s_instance_type
+  subnet_id = aws_subnet.cluster_subnet.id
+  associate_public_ip_address = true
+  key_name = "cluster-key"
+  tags = {
+    Name = "k8s-node-${count.index + 1}"
+  }
+  lifecycle {
+    ignore_changes = [associate_public_ip_address]
   }
 }
--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,5 +1,5 @@
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "cluster_vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
     Name = "Training"
   }
